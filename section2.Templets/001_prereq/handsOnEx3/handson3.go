49c20db2b0ac2894a4f9840479f5af921282de53
borkab <borbala.botond@gmail.com>
2024-03-26 21:18:34 +0100

    .

--- section2.Templets/001_prereq/handsOnEx3/handson3.go
+++ section2.Templets/001_prereq/handsOnEx3/handson3.go
@@ -1,1 +1,24 @@
 package main
+
+type Person struct {
+	fName string
+	lName string
+}
+
+type SecretAgent struct {
+	Person
+	licenceToKill bool
+}
+
+func (p Person) pSpeak() string {
+	return "I am " + p.fName + " " + p.lName
+}
+
+func (sa SecretAgent) saSpeak() string {
+
+	if sa.licenceToKill { //if sa.licenceToKill == true   -> you can simplify it
+		return "My name is " + sa.fName + " " + sa.lName + " and I am a secret agent"
+	} else {
+		return "My name is " + sa.fName + " " + sa.lName + " and I am NOT a secret agent"
+	}
+}
